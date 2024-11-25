ae0a69f4581efb1effef66d4b9c98b7aafbbf08d
Francisco Flores <francisco.giavay@gmail.com>
2024-11-25 16:38:19 -0300

    first commit

---
+++ src/main/java/com/personal/turns/TurnsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.turns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnsApplication.class, args);
+	}
+
+}
